f4d8c13e364c5a7b84b179318c90b4b3ff1ca32b
mdonstad <michael@avlife.com>
2024-02-29 10:38:50 -0800

    build plugin

---
+++ plugins/netlify-app-main-config/index.js
@@ -0,0 +1,16 @@
+    export const onPreBuild = async ({ constants }) => {
+        const store = getDeployStore({
+          siteID: constants.SITE_ID,
+          token: constants.NETLIFY_API_TOKEN,
+        });
+        await store.set("appCS", "For App Config",{
+            metadata: {
+                name: 'Smart Mixers',
+                themeDark: 'hsl(199.7 85.9% 47.7%)',
+                themeLight: 'hsl(198.6 88.7% 48.4%)',
+                color: 'hsl(199.7 85.9% 45.7%)',
+                stytchProjectID: import.meta.env.STYTCH_PROJECT_ID,
+                StytchToken: import.meta.env.STYTCH_PUBLIC_TOKEN
+            }
+        });
+    };
